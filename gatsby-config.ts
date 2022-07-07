bc755df736bf0d7043fb888c15aa7d0cb24b4b5a
Bruno Gregório <Bruno08new12@gmail.com>
2022-07-07 11:29:04 -0300

    Base toolchain working.

---
+++ gatsby-config.ts
@@ -0,0 +1,27 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `Funk Forward`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
+  // If you use VSCode you can also use the GraphQL plugin
+  // Learn more at: https://gatsby.dev/graphql-typegen
+  graphqlTypegen: true,
+  plugins: ["gatsby-plugin-sass", "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
+    },
+    __key: "images"
+  }]
+};
+
+export default config;
---
+++ src/pages/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import Component from '../components/test'
+
+export default function Page() {
+  return <Component />
+}
