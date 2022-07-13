c99985b998b71a2f8f0c484d645033f9deb7968f
Bruno Gregório <Bruno08new12@gmail.com>
2022-07-13 14:47:20 -0300

    Added prototyping page.

---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { FollowMe } from '../components/FollowMe'
+import '../styles/global.scss'
+
+export default function Page() {
+  return (
+    <>
+      <FollowMe />
+    </>
+  )
+}
