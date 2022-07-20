24a97691f0d10a826b3f34e01e90be695d19b832
Bruno Gregório <Bruno08new12@gmail.com>
2022-07-20 16:45:51 -0300

    BottomBar ready.

--- src/components/BottomBar/index.tsx
+++ src/components/BottomBar/index.tsx
@@ -3,7 +3,6 @@ import { setDefaultClasses, filterProps } from '../../utils/props'
 import { SVG } from '../SVG'
 import { FollowMe } from '../FollowMe'
 
-import Logo from '../../images/logo-horizontal.svg'
 import * as styles from './BottomBar.module.scss'
 
 type BottomBarProps = HTMLProps<HTMLDivElement>
@@ -13,7 +12,7 @@ export function BottomBar(props: BottomBarProps) {
   return (
     <div {...htmlProps}>
       <div className={styles.internalGrid}>
-        <SVG icon={Logo} className={styles.logo} />
+        <SVG icon='logo-horizontal' colorize className={styles.logo} />
         <div>All rights reserved &copy; {(new Date()).getUTCFullYear()}</div>
         <FollowMe light />
       </div>
