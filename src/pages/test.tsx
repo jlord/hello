4b599768f64085b74d2c7aeca5ed5f61fc278709
Bruno Gregório <Bruno08new12@gmail.com>
2022-07-20 17:15:05 -0300

    First iteration for the player component.

--- src/pages/test.tsx
+++ src/pages/test.tsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import { BottomBar } from '../components/BottomBar'
+// import { BottomBar } from '../components/BottomBar'
 // import { Introduction } from '../templates/Introduction'
+import { Player } from '../components/Player'
 import '../styles/global.scss'
 
 export default function Page() {
   return (
     <>
-      <BottomBar />
+      <Player />
     </>
   )
 }
