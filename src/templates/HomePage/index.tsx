875e9739b8f99818699a65a902236bad45011014
Bruno Gregório <Bruno08new12@gmail.com>
2022-08-27 16:59:02 -0300

    Added carousel to homepage prototype.

--- src/templates/HomePage/index.tsx
+++ src/templates/HomePage/index.tsx
@@ -4,16 +4,40 @@ import { useBackgroundImage } from '../../utils/hooks/useImage'
 import { useSvgUrl } from '../../components/SVG/hooks'
 import { Player } from '../../components/Player'
 import { BottomBar } from '../../components/BottomBar'
+import { Carousel } from '../../components/Carousel'
 import { Introduction } from '../Introduction'
 import * as styles from './HomePage.module.scss'
 
 export function HomePage() {
   const link = useSvgUrl('page-background-scalable')
   const background = useBackgroundImage(link)
+
+  // For test purposes
+  const musics = [
+    {
+      name: '01. Track 1',
+      album: 'Test Album',
+      link: '#track1',
+      toRelease: false
+    },
+    {
+      name: '02. Track 2',
+      album: 'Test Album',
+      link: '#track2',
+      toRelease: false
+    },
+    {
+      name: '03. Track 3',
+      album: 'Test Album',
+      link: '#track3',
+      toRelease: true
+    }
+  ]
   return (
     <div className={styles.container} style={background}>
       <div className={styles.intro}>
         <Introduction />
+        <Carousel musics={musics} />
       </div>
       <Player />
       <BottomBar />
