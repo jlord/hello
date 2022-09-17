8c1793333179257d2363510c78ed1192789cb61f
Bruno Gregório <Bruno08new12@gmail.com>
2022-09-17 10:33:47 -0300

    Added video hook.

---
+++ src/utils/hooks/useVideo.ts
@@ -0,0 +1,46 @@
+import { useStaticQuery, graphql } from 'gatsby'
+
+const getVideosQuery = graphql`
+  query GetVideos {
+    allFile(filter: { extension: { regex: "/(mp4)|(webm)/" } }) {
+      edges {
+        node {
+          publicURL
+          name
+          extension
+        }
+      }
+    }
+  }
+`
+
+type AvailableVideos = 'GTO90-Scene-001-New'
+type QueryReturnType = {
+  allFile: {
+    edges: {
+      node: {
+        publicURL: string
+        name: string
+        extension: string
+      }
+    }[]
+  }
+}
+
+export function useVideo(videoName: AvailableVideos) {
+  const data: QueryReturnType = useStaticQuery(getVideosQuery)
+  const videos = data.allFile.edges
+    .filter((edge) => edge.node.name === videoName)
+    .map((edge) => edge.node)
+  const defaultVideoUrl = videos[0].publicURL
+
+  if (videos.length > 1) {
+    const webmVersion = videos.find(
+      (video) => video.extension === 'webm'
+    )?.publicURL
+
+    return webmVersion ?? defaultVideoUrl
+  }
+
+  return defaultVideoUrl
+}
