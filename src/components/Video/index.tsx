c7c9e8fcef253530bd48c2afdb272334fd8d7abd
Bruno Gregório <Bruno08new12@gmail.com>
2022-09-17 11:19:18 -0300

    Added video component.

---
+++ src/components/Video/index.tsx
@@ -0,0 +1,38 @@
+import React, { type HTMLProps, useId, useState } from 'react'
+import { composeProps } from '../../utils/props'
+import { Overlay } from './components/Overlay'
+import * as styles from './Video.module.scss'
+
+type VideoProps = HTMLProps<HTMLDivElement> & {
+  src: string
+}
+
+export function Video(props: VideoProps) {
+  const { src } = props
+  const htmlProps = composeProps(
+    props,
+    ['children', 'ref', 'src'],
+    [styles.container]
+  )
+
+  const [showOverlay, setOverlay] = useState(true)
+
+  const videoId = useId()
+  const toggleVideo = () => {
+    const videoElement = document.getElementById(videoId) as HTMLVideoElement
+    if (videoElement.paused) {
+      setOverlay(false)
+      videoElement.play()
+    } else {
+      setOverlay(true)
+      videoElement.pause()
+    }
+  }
+
+  return (
+    <div {...htmlProps} onClick={toggleVideo} ref={props.ref}>
+      {showOverlay && <Overlay />}
+      <video src={src} id={videoId} />
+    </div>
+  )
+}
