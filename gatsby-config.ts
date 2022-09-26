292d477e364984555600601bbd76b5525c50bbd3
Bruno Gregório <Bruno08new12@gmail.com>
2022-09-26 07:26:03 -0300

    Updated deployment script.

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -9,6 +9,7 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
+  pathPrefix: '/beepbeep',
   plugins: [
     'gatsby-plugin-sass',
     'gatsby-plugin-image',
