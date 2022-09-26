1dfc8ab165b584dbb75d02a66500bf0b6d41ef32
Bruno Gregório <Bruno08new12@gmail.com>
2022-09-26 09:30:33 -0300

    Responsive carousel.

--- src/components/Video/youtube.tsx
+++ src/components/Video/youtube.tsx
@@ -1,23 +1,23 @@
 import React, { type HTMLProps } from 'react'
 import { composeProps } from '../../utils/props'
+import * as styles from './youtube.module.scss'
 
 type YoutubeProps = {
   videoId?: string
-} & HTMLProps<HTMLIFrameElement>
+} & HTMLProps<HTMLDivElement>
 
 export function Youtube(props: YoutubeProps) {
   const { videoId = 'fiGuXsvy69E' } = props
-  const htmlProps = composeProps(props, ['videoId'])
+  const htmlProps = composeProps(props, ['videoId'], [styles.container])
 
   return (
-    <iframe
-      width="315"
-      height="315"
-      src={`https://www.youtube.com/embed/${videoId}?controls=0`}
-      title="YouTube video player"
-      frameBorder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope"
-      {...htmlProps}
-    ></iframe>
+    <div {...htmlProps}>
+      <iframe
+        src={`https://www.youtube.com/embed/${videoId}?controls=0`}
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope"
+      ></iframe>
+    </div>
   )
 }
